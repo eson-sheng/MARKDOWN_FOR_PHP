7e322a489cb4ecb043e4f4ab9b078c552f63a821
eson <834767372@qq.com>
2019-02-13 16:06:22 +0800

    【更新】- 增加新的PHP解析markdown库

--- md_low.php
+++ md_low.php
@@ -3,14 +3,15 @@ spl_autoload_register(function($class){
     require str_replace('\\', DIRECTORY_SEPARATOR, ltrim("php-markdown/{$class}", '\\')).'.php';
 });
 
-use Michelf\Markdown;
+use HyperDown\Parser;
 
 if (empty($_GET['file'])) {
     exit("<html><head><title>404 Not Found</title></head><body><center><h1>404 Not Found</h1></center><hr><center>nginx</center></body></html>");
 }
 
+$parser = new HyperDown\Parser;
 $text = file_get_contents($_GET['file']);
-$html = Markdown::defaultTransform($text);
+$html = $parser->makeHtml($text);
 
 ?>
 <!DOCTYPE html>
